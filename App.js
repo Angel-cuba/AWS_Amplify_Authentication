f4ae29e8fd2aa90349a3a0bc5823d5cd4e1e2c6e
Mac_Developer <araozangel842@gmail.com>
2022-08-04 15:14:21 +0300

    Installing dependencies

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import SigInScreen from './src/screens/SigInScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <SigInScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
