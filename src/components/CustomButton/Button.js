06914ccd8aaf070fff2096a12b8331e84c0503fe
Mac_Developer <araozangel842@gmail.com>
2022-08-04 18:40:08 +0300

    SigInScreen DONE

---
+++ src/components/CustomButton/Button.js
@@ -0,0 +1,50 @@
+import {Text, StyleSheet, Pressable} from 'react-native';
+import React from 'react';
+
+const Button = ({onPress, text, type = 'first', bgColor, fgColor}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        styles.container,
+        styles[`container_${type}`],
+        bgColor ? {backgroundColor: bgColor} : {},
+      ]}>
+      <Text
+        style={[
+          styles.text,
+          styles[`text_${type}`],
+          fgColor ? {color: fgColor} : {},
+        ]}>
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  container_first: {
+    backgroundColor: '#3B71F3',
+  },
+
+  container_third: {
+    borderColor: '#ccc',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text_first: {},
+  text_third: {
+    color: '#000000',
+  },
+});
+export default Button;
--- src/screens/SigInScreen.js
+++ src/screens/SigInScreen.js
@@ -1,6 +1,13 @@
-import {View, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import Input from '../components/CustomInput/Input';
+import Button from '../components/CustomButton/Button';
 import Logo from '../../assets/angel_Dev.png';
 
 const SigInScreen = () => {
@@ -8,21 +15,71 @@ const SigInScreen = () => {
 
   const [userName, setUserName] = React.useState('');
   const [password, setPassword] = React.useState('');
+
+  const onSigInPress = () => {
+    console.warn('SigIn');
+  };
+  const onForgotPasswordPress = () => {
+    console.warn('ForgotPassword');
+  };
+  const onSignInWithFacebook = () => {
+    console.warn('SignInWithFacebook');
+  };
+  const onSignInWithGoogle = () => {
+    console.warn('SignInWithGoogle');
+  };
+  const onSignInWithApple = () => {
+    console.warn('SignInWithApple');
+  };
+  const onDontHaveAccount = () => {
+    console.warn('DontHaveAccount');
+  };
   return (
-    <View style={styles.root}>
-      <Image
-        source={Logo}
-        style={[styles.logo, {height: height * 0.3}]}
-        resizeMode="contain"
-      />
-      <Input placeholder="Username" value={userName} setValue={setUserName} />
-      <Input
-        placeholder="Password"
-        value={password}
-        setValue={setPassword}
-        secureTextEntry
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, {height: height * 0.3}]}
+          resizeMode="contain"
+        />
+        <Input placeholder="Username" value={userName} setValue={setUserName} />
+        <Input
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+        <Button text="Sign In" onPress={onSigInPress} />
+        <Button
+          text="ForgotPassword"
+          onPress={onForgotPasswordPress}
+          type="third"
+        />
+        <Button
+          text="SignIn with Facebook"
+          onPress={onSignInWithFacebook}
+          bgColor="#E7EAF4"
+          fgColor="#4765A9"
+        />
+        <Button
+          text="SignIn with Google"
+          onPress={onSignInWithGoogle}
+          bgColor="#FAE9EA"
+          fgColor="#DD4D44"
+        />
+        <Button
+          text="SignIn with Apple"
+          onPress={onSignInWithApple}
+          bgColor="#e3e3e3"
+          fgColor="#363636"
+        />
+        <Button
+          text="Don't have an account? SignUp"
+          onPress={onDontHaveAccount}
+          type="third"
+        />
+      </View>
+    </ScrollView>
   );
 };
 
